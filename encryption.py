6adcb6033a647bfa93dd388e3612b2ce1336c55e
gavriel miyara <gabimiyara1@gmail.com>
2025-09-03 14:26:45 +0300

    Add encryption and decryption methods to handle data securely

---
+++ encryption.py
@@ -0,0 +1,26 @@
+class Encrption:
+    @staticmethod
+    def encrypt(data,key):
+        result = ""
+        i = 0
+        while len(data) > i:
+            for char in key:
+                if i < len(data):
+                    result += chr(ord(data[i]) ^ ord(char))
+                    i += 1
+                else:
+                    break
+        return result
+    def decrypt(data,key):
+        return Encrption.encrypt(data,key)
+    
+
+
+for i in range(1000):
+    print(chr(i),end=" ")
+    
+
+
+            
+
+
